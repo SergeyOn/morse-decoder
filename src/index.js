5f803efae52f24ed1836a9018a312ddd62a442bf
SergeyOn <seregapantera@gmail.com>
2019-09-22 02:10:10 +0300

    morse done 52ms

--- src/index.js
+++ src/index.js
@@ -37,8 +37,29 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const reverse = str => str.split('').reverse().join(''); //helper function
+
 function decode(expr) {
-    // write your solution here
+    let result = ''; // result sentence ex: hello world
+    for (let i = 9, length = expr.length; i < length; i += 10) { // cycle for 10 symbols = 1 letter
+        let letter = ''; // letter in code ex: ..-., but in reverse order
+        for (let j = i; j !== i-10; j -= 2 ) { //cycle code from the end
+            if (expr[j-1] === '0' || expr[j] === '*') break; // 00 or 'space'
+           
+            if (expr[j] === '1' && expr[j-1] === '1' ) {
+                letter += '-';
+            } else {
+                letter += '.';
+            }
+        }
+        
+        if (!letter) {
+          result += ' '; //add space
+        } else {
+          result += MORSE_TABLE[reverse(letter)]; // add reversed letter-code from table
+        }
+    }
+    return result;
 }
 
 module.exports = {
